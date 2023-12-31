493ee3ca3e87c159ec1492623eec575c1e396ae6
劉敦桐 <zse811022@hotmail.com>
2023-12-31 10:19:26 +0800

    Test

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+
+		}
+	}
+}
---
+++ WebApplication1/Service/messageDBService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using WebApplication1.Models;
+
+namespace WebApplication1.Service
+{
+	public class messageDBService
+	{
+		public WebApplication1.Models.db1Entities db =new Models.db1Entities();
+		public List<Article> GetData()
+		{
+			return(db.Articles.ToList());
+		}
+
+		public void DBCreate(string Article_title, string Content)
+		{
+			Article New = new Article();
+			New.Title=Article_title;
+			New.Content=Content;
+			New.time=DateTime.Now;
+			db.Articles.Add(New);
+			db.SaveChanges();
+		}
+	}
+}
